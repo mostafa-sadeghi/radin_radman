9a962e673583977f837102011e0c57826ced7fa5
msa <msai.sadehi@gmail.com>
2023-06-26 20:58:14 +0330

    snake

--- snake_game.py
+++ snake_game.py
@@ -1,22 +1,33 @@
-from turtle import Screen
 from snake_game_utils import *
+from time import sleep
+
 
 def on_close():
     global running
     running = False
 
-main_surface = Screen()
-main_surface.bgcolor('blue')
-main_surface.setup(width=600, height=600)
-main_surface.title("Snake Game")
+
+def go_up():
+    snake_head.direction = "up"
+
+
+main_surface = make_screen("blue", "snake game", 600, 600)
 main_surface.tracer(False)
 
 root = main_surface._root
 root.protocol('WM_DELETE_WINDOW', on_close)
 
 snake_head = make_turtle("square", "black")
-snake_head.goto(100,100)
+snake_head.direction = ""
 
+snake_food = make_turtle("circle", "red")
+change_food_position(snake_food)
+
+main_surface.listen()
+main_surface.onkeypress(go_up, "w")
+# TODO  برای سایر جهت ها
 running = True
 while running:
     main_surface.update()
+    move_snake(snake_head)
+    sleep(0.2)
--- snake_game_utils.py
+++ snake_game_utils.py
@@ -1,4 +1,8 @@
 from turtle import Turtle
+from turtle import Screen
+from random import randint
+
+
 def make_turtle(tshape, tcolor):
     my_turtle = Turtle()
     my_turtle.shape(tshape)
@@ -7,6 +11,23 @@ def make_turtle(tshape, tcolor):
     my_turtle.speed("fastest")
     return my_turtle
 
-def make_screen():
-    pass
-    # TODO
+
+def make_screen(color, title, w, h):
+    window = Screen()
+    window.bgcolor(color)
+    window.title(title)
+    window.setup(width=w, height=h)
+    return window
+
+
+def change_food_position(food):
+    x = randint(-270, 270)
+    y = randint(-270, 270)
+    food.goto(x, y)
+
+
+def move_snake(snake_head):
+    if snake_head.direction == "up":
+        ypos = snake_head.ycor()
+        ypos += 20
+        snake_head.sety(ypos)
