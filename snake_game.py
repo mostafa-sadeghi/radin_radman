4941a8c8f331a2effb3d66f945deb532d49a8338
msa <msai.sadehi@gmail.com>
2023-07-10 20:58:49 +0330

    sadeghi

--- snake_game.py
+++ snake_game.py
@@ -1,6 +1,7 @@
 from snake_game_utils import *
 from time import sleep
 
+score = 0
 
 def on_close():
     global running
@@ -9,6 +10,18 @@ def on_close():
 
 def go_up():
     snake_head.direction = "up"
+
+
+def go_down():
+    snake_head.direction = "down"
+
+
+def go_left():
+    snake_head.direction = "left"
+
+
+def go_right():
+    snake_head.direction = "right"
 
 
 main_surface = make_screen("blue", "snake game", 600, 600)
@@ -23,11 +36,31 @@ snake_head.direction = ""
 snake_food = make_turtle("circle", "red")
 change_food_position(snake_food)
 
+score_board = make_turtle("square", "white")
+score_board.ht()
+score_board.goto(0,260)
+
+
+
 main_surface.listen()
+main_surface.onkeypress(go_up, "Up")
+main_surface.onkeypress(go_down, "Down")
+main_surface.onkeypress(go_left, "Left")
+main_surface.onkeypress(go_right, "Right")
 main_surface.onkeypress(go_up, "w")
-# TODO  برای سایر جهت ها
+main_surface.onkeypress(go_down, "s")
+main_surface.onkeypress(go_left, "a")
+main_surface.onkeypress(go_right, "d")
 running = True
 while running:
+    score_board.clear()
+    score_board.write(f"Score : {score}",align="center",\
+                       font=("Terminal", 22, "normal"))
     main_surface.update()
+    if snake_head.distance(snake_food) < 20:
+        change_food_position(snake_food)
+        score += 1
+
+
     move_snake(snake_head)
     sleep(0.2)
--- snake_game_utils.py
+++ snake_game_utils.py
@@ -3,6 +3,14 @@ from turtle import Screen
 from random import randint
 
 
+def make_screen(color, title, w, h):
+    window = Screen()
+    window.bgcolor(color)
+    window.title(title)
+    window.setup(width=w, height=h)
+    return window
+
+
 def make_turtle(tshape, tcolor):
     my_turtle = Turtle()
     my_turtle.shape(tshape)
@@ -12,17 +20,9 @@ def make_turtle(tshape, tcolor):
     return my_turtle
 
 
-def make_screen(color, title, w, h):
-    window = Screen()
-    window.bgcolor(color)
-    window.title(title)
-    window.setup(width=w, height=h)
-    return window
-
-
 def change_food_position(food):
     x = randint(-270, 270)
-    y = randint(-270, 270)
+    y = randint(-270, 230)
     food.goto(x, y)
 
 
@@ -31,3 +31,18 @@ def move_snake(snake_head):
         ypos = snake_head.ycor()
         ypos += 20
         snake_head.sety(ypos)
+
+    if snake_head.direction == "down":
+        ypos = snake_head.ycor()
+        ypos -= 20
+        snake_head.sety(ypos)
+
+    if snake_head.direction == "left":
+        xpos = snake_head.xcor()
+        xpos -= 20
+        snake_head.setx(xpos)
+
+    if snake_head.direction == "right":
+        xpos = snake_head.xcor()
+        xpos += 20
+        snake_head.setx(xpos)
